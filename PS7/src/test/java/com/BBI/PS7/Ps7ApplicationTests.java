a3862016234168130704f0b8b7148fc27f40e148
CodingMachin3 <42521800+CodingMachin3@users.noreply.github.com>
2023-12-16 11:27:19 +0530

    Add files via upload
    commit one

---
+++ PS7/src/test/java/com/BBI/PS7/Ps7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BBI.PS7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
